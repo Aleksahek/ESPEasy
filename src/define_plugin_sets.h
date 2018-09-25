50359840329e3c2584020cfb6315156b1fb1d664
Andrey2509 <andrey2509@yandex.ru>
2018-09-25 09:27:46 +0500

    Added plugin _P220_Mercury230.ino "Energy meter"

--- src/define_plugin_sets.h
+++ src/define_plugin_sets.h
@@ -575,6 +575,8 @@ To create/register a plugin, you have to :
 	#define USES_P211	// MPU6050
 	#define USES_P212	// MY9291
 	#define USES_P213	// VEML6070
+        #define USES_P220	//Mercury 230
+
 #endif
 
 
